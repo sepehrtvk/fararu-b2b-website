75bd2e000db5ba1dc1045aad169b23bf8e2f881b
Sepehr Tavakoli <sepehrtvk2001@gmail.com>
2023-05-27 13:18:54 +0330

    add header categories

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //Styles
 import styles from "./Header.module.css";
@@ -16,13 +16,47 @@ import { logout } from "../../store/slices/user";
 import { clearCustomer } from "../../store/slices/customer";
 import { deleteBasket } from "../../store/slices/basket";
 import { toLocaleNumberString } from "../../common/Localization";
+import { getProductGroups } from "../../api/product";
+import {
+  ProductGroupModel,
+  ProductGroupTwoLevelModel,
+} from "../../api/product/types";
+import ProductGroupItem from "./ProductGroupItem/ProductGroupItem";
 
 const Header = () => {
   const [menu, setMenu] = useState(false);
+  const [productGroupTwoLevel, setProductGroupTwoLevel] = useState<
+    ProductGroupTwoLevelModel[]
+  >([]);
   const totalQty = useAppSelector((state) => selectBasketSKU(state));
   const isLoggedIn = !!useAppSelector((state) => state.user.token);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const subscription = getProductGroups().subscribe({
+      next: (tree: ProductGroupModel[]) => {
+        const productGroupTwoLevelTemp: ProductGroupTwoLevelModel[] = [];
+
+        tree.map((row) => {
+          row.submenus?.map((sub) => {
+            const temp: ProductGroupTwoLevelModel = {
+              firstLevel: null,
+              secondLevel: [],
+            };
+            // if (sub.nLevel == 1) {
+            temp.firstLevel = sub;
+            if (sub.submenus) temp.secondLevel = sub.submenus;
+            //}
+            productGroupTwoLevelTemp.push(temp);
+          });
+        });
+        setProductGroupTwoLevel(productGroupTwoLevelTemp);
+      },
+      error: () => {},
+    });
+    return () => subscription.unsubscribe();
+  }, []);
 
   return (
     <header className={styles.header}>
@@ -33,11 +67,12 @@ const Header = () => {
             onClick={() => setMenu(!menu)}>
             <img src={menu ? cross : hamburger} alt='hamburger' />
           </div>
+
           <div className={styles.logo}>
             <img width={"70px"} src={logo} alt='logo' />
           </div>
           <div className={styles.search}>
-            <input type='search' placeholder='جستجو...' />
+            <input type='search' placeholder='جستجو در فروشگاه...' />
             <button>
               <img src={searchIcon} alt='search' />
             </button>
@@ -80,26 +115,17 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className={styles.border}></div>
-
       <div className={styles.container}>
         <div className={styles.bottomheader}>
           <ul className={menu ? styles.navbaropen : styles.navbar}>
-            <li className={styles.navitem}>
-              <Link to='/home'>صفحه اصلی</Link>
-            </li>
-            <li className={styles.navitem}>
-              <Link to='/shop'>فروشگاه</Link>
-            </li>
-            {/* <li className={styles.navitem}>
-              <Link to='/blogs'>مقالات</Link>
-            </li> */}
-            <li className={styles.navitem}>
-              <Link to='/aboutus'>درباره ما</Link>
-            </li>
-            <li className={styles.navitem}>
-              <Link to='/contactus'>تماس با ما</Link>
-            </li>
+            {productGroupTwoLevel.map((level1, index) => {
+              if (index < 5)
+                return (
+                  <li className={styles.navitem}>
+                    <ProductGroupItem productGroupTwoLevel={level1} />
+                  </li>
+                );
+            })}
           </ul>
         </div>
       </div>
